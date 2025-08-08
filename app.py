eed6c627366444de5db8bc50380d8ddd311a3b6f
EdwardJDS <eduardo.shinnoda@gmail.com>
2025-08-08 14:11:07 -0300

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as pxx
+import plotly.express as px
 
 # --- Configuração da Página ---
 # Define o título da página, o ícone e o layout para ocupar a largura inteira.
@@ -137,3 +137,4 @@ with col_graf4:
 st.subheader("Dados Detalhados")
 
 st.dataframe(df_filtrado)
+
